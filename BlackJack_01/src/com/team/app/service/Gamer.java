723131f4b49bee960b02df82607160e50ec77bb3
kimyoungjin98 <gyu250@naver.com>
2021-04-18 10:55:14 +0900

    player

--- BlackJack_01/src/com/team/app/service/Gamer.java
+++ BlackJack_01/src/com/team/app/service/Gamer.java
@@ -1,9 +1,15 @@
 package com.team.app.service;
 
+import com.team.app.model.CardVO;
+
 public interface Gamer {
 
 	public void getCard();
 	public void openCard();
 	public Integer sumPoint();
+	
+
+
+	
 	
 }
