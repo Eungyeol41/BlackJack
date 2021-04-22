c52dd24d38074c91fe609623e4c1669b3aed9281
Nanask <ybhag1005@daum.net>
2021-04-22 14:24:38 +0900

    푸쉬할게여

---
+++ BlackJack_01/src/com/team/app/service/impl/GameRule.java
@@ -0,0 +1,13 @@
+package com.team.app.service.impl;
+
+public class GameRule {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Game Rule");
+		System.out.println("Dealer와 Player가 있다");
+		System.out.println("카드는 조커를 제외한 52장이다");
+		System.out.println("카드는 ");
+		
+	}
+}
