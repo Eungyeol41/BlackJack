e77cf36c93aded1199b6752c842208b5f91a2e9b
Eungyeol41 <iniziato@naver.com>
2021-04-23 16:22:57 +0900

    오류 수정

--- BlackJack_01/src/com/team/app/service/impl/Player.java
+++ BlackJack_01/src/com/team/app/service/impl/Player.java
@@ -179,45 +179,13 @@ public class Player implements Gamer {
 			pMoney -= inputBet;
 			break;
 		}
-		this.intBet += inputBet;
+		intBet += inputBet;
 		System.out.println("-".repeat(50));
 		System.out.println("베팅 금액 : " + intBet);
 		System.out.println("소지 금액 : " + pMoney);
 
 	}
 
-	protected Integer bBetting() {
-
-		while (true) {
-			System.out.println("-".repeat(50));
-			System.out.println("베팅할 액수를 입력해주세요");
-			System.out.print(">> ");
-			String strBet = scan.nextLine();
-
-			try {
-				bBet = Integer.valueOf(strBet);
-			} catch (NumberFormatException e) {
-				System.out.println("정수만 입력하세요 !!");
-				continue;
-			}
-
-			if (bBet > pMoney) {
-				System.out.println("입력한 액수가 소지금액보다 큽니다 !!");
-				continue;
-			} else if (bBet < 0) {
-				System.out.println("0보다 큰 액수를 입력하세요 !!");
-				continue;
-
-			}
-			pMoney -= bBet;
-			break;
-		}
-		System.out.println("-".repeat(50));
-		System.out.println("베팅 금액 : " + bBet);
-		System.out.println("소지 금액 : " + pMoney);
-
-		return bBet;
-	}
 
 	public void saveMoney() {
 
